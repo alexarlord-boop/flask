12ca5b3fef863a35d21212ce5c6bf174c0e72415
alexarlord-boop <55353828+alexarlord-boop@users.noreply.github.com>
2020-03-07 15:02:06 +0300

    promo image

--- 1.py
+++ 1.py
@@ -13,20 +13,32 @@ def index():
     return "<h1>И на Марсе будут яблони цвести!</h1>"
 
 
-@app.route('/promotion')
+@app.route('/promotion_image')
 def promo():
-    return """<!doctype html>
+    return f"""<!doctype html>
                 <html lang="en">
                   <head>
                     <meta charset="utf-8">
                     <title>promo</title>
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
+                    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" 
+                    crossorigin="anonymous">
+                    <link rel="stylesheet" href={url_for('static', filename='css/style.css')}
                   </head>
                   <body>
-                    <h1>Человечество вырастает из детства.</h1>
-                    <h1>Человечеству мала одна планета.</h1>
-                    <h1>Мы сделаем обитаемыми безжизненные пока планеты.</h1>
-                    <h1>И начнем с Марса!</h1>
-                    <h1>Присоединяйся!</h1>
+                  <div class="Top"><h1>Жди нас, Марс!</h1></div>
+                            <div>
+                                <img src="{url_for('static', filename='img/mars.png')}"'''))
+                                <h3>Вот она какая, красная планета.</h3>
+                            </div>
+
+            <div class="alert alert-info h1" role="alert">Человечество вырастает из детства.</div>
+            <div class="alert alert-success h1" role="alert">Человечеству мала одна планета.</div>
+            <div class="alert alert-info h1" role="alert">Мы сделаем обитаемыми безжизненные пока планеты.</div>
+            <div class="alert alert-warning h1" role="alert">И начнем с Марса!</div>
+            <div class="alert alert-danger h1" role="alert">Присоединяйся!</div>
                    
                   </body>
                 </html>"""
@@ -41,11 +53,7 @@ def mars():
                         <title>Привет, Марс!</title>
                       </head>
                       <body>
-                        <h1>Жди нас, Марс!</h1>
-                            <div>
-                                <img src="{url_for('static',filename='img/mars.png')}"'''))
-                                <h3>Вот она какая, красная планета.</h3>
-                            </div>
+                        
 
                       </body>
                     </html>"""
